fb5022e034703f724b48c3cc030ef67d2e572072
Yadav <byadav@jcp.com>
2019-12-08 23:49:42 +0530

    Initial stage of App.

---
+++ CBS/app/src/main/java/com/cbs/ui/share/ShareViewModel.java
@@ -0,0 +1,19 @@
+package com.cbs.ui.share;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class ShareViewModel extends ViewModel {
+
+    private MutableLiveData<String> mText;
+
+    public ShareViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("Coming soon...");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
